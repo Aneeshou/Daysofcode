4e2817f0b6ff38138bde469d0a6049d813f271a1
Aayushman <aayushmanji11@gmail.com>
2025-09-01 20:53:15 +0530

    q23.c

---
+++ q23.c
@@ -0,0 +1,43 @@
+// Write a program to calculate a library fine based on late days.
+
+/*
+Sample Test Cases:
+Input 1:
+4
+Output 1:
+Fine ₹8
+
+Input 2:
+8
+Output 2:
+Fine ₹32
+
+Input 3:
+15
+Output 3:
+Fine ₹90
+
+Input 4:
+31
+Output 4:
+Membership Cancelled
+
+*/
+#include <stdio.h>
+
+int main() {
+    int daysLate,fine;
+    printf("Enter the number of days the book is late: ");
+    scanf("%d", &daysLate);
+     if (daysLate <= 0) {
+        fine = 0;
+         } else if (daysLate <= 5) {
+        fine = daysLate * 1;
+         } else if (daysLate <= 10) {
+             fine = 10 + ((daysLate - 5));
+              } else {
+                fine = 10 +10 + ((daysLate - 10)*5);}
+                  printf("The fine for %d days late is Rs %d", daysLate, fine);
+
+    return 0;
+}
