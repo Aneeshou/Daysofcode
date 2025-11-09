1fdd530fd8efe3ffadc1088d55cd661587be6cb7
Aayushman <aayushmanji11@gmail.com>
2025-11-09 14:03:20 +0530

    q65.c

---
+++ q65.c
@@ -0,0 +1,51 @@
+//Q65: Search in a sorted array using binary search.
+
+/*
+Sample Test Cases:
+Input 1:
+5
+1 3 5 7 9
+7
+Output 1:
+Found at index 3
+
+Input 2:
+5
+1 3 5 7 9
+6
+Output 2:
+-1
+
+*/
+#include <stdio.h>
+
+int binarySearch(int arr[], int size, int target) {
+    int low = 0;
+    int high = size - 1;
+    while (low <= high) {
+        int mid = low + (high - low) / 2; // Avoids overflow 
+
+        if (arr[mid] == target) {
+            return mid;
+        } else if (arr[mid] < target) {
+            low = mid + 1; //the right half
+        } else {
+            high = mid - 1; //left half
+        }
+    }
+    return -1;
+}
+int main() {
+    int arr[] = {2, 5, 8, 12, 16, 23, 38, 56, 72, 91};
+    int size = sizeof(arr) / sizeof(arr[0]);
+    int target = 23;
+
+    int result = binarySearch(arr, size, target);
+
+    if (result != -1) {
+        printf("Element %d found at index %d\n", target, result);
+    } else {
+        printf("Element %d not found in the array\n", target);
+    }
+    return 0;
+}
